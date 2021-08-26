9413098a1b3ae4c5233bab0b4077de6ffde56ace
Laerso Ramos <laerso.r@gmail.com>
2021-08-26 17:44:07 -0300

    preparação para pegar dados da API Marvel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const initialURL ="https://gateway.marvel.com:443/v1/public/characters?ts=146097&apikey=c3be1e0fe9ab77d727ab9e0496108c6f&hash=cda3c0e1591878e1e1b29c52fecc8269"
+  const [char, setChar] = useState([])
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setChar(data.data.results)
+      })
+      .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialURL)
+  }, [])
+
+  console.log(char)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ul>
+          {char.map((list, index) => 
+            <li key={index}>
+              {list.name}
+              <img src={list.thumbnail.path} alt="" />
+            </li> )}
+        </ul>
+      </div>
+      
     </div>
   );
 }
